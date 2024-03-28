1c643db4ab6f3d9f332d32403c74fc7ef1becec6
Gavin Schroeder <gschroeder8@my.wctc.edu>
2024-03-28 01:38:46 -0500

    movie class with getters and setters

---
+++ Movie.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+public class Movie
+{
+    public ulong MediaId { get; set; }
+    public string Title { get; set; }
+    public List<string> Genres { get; set; }
+    public string Director { get; set; }
+    public TimeSpan RunningTime { get; set; }
+}
